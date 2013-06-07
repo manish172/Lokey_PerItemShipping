6a91247ad084618c5a300cd77eb87f3113f125f9
Lee Saferite <lee.saferite@lokeycoding.com>
2013-06-07 12:29:54 -0400

    Convert code to an open source license

--- app/code/local/Lokey/PerItemShipping/Helper/Data.php
+++ app/code/local/Lokey/PerItemShipping/Helper/Data.php
@@ -2,13 +2,13 @@
 /**
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Lokey Coding, LLC - SOFTWARE LICENSE (v1.0)
- * that is bundled with this package in the file LICENSE.txt
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file OSL_LICENSE.txt
  *
  * @category   Mage
  * @package    Lokey_PerItemShipping
- * @copyright  Copyright (c) 2009 Lokey Coding, LLC <ip@lokeycoding.com>
- * @license    Lokey Coding, LLC - SOFTWARE LICENSE (v1.0)
+ * @copyright  Copyright (c) 2009-2013 Lokey Coding, LLC <ip@lokeycoding.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @author     Lee Saferite <lee.saferite@lokeycoding.com>
  */
 
